598e6952bec293c78171c58ef9439c95093288d2
Jan She <janmarkshe@gmail.com>
2018-07-04 22:55:53 +0800

    end of july 4, 2018 coding day

--- js/script.js
+++ js/script.js
@@ -6,12 +6,21 @@ $(document).ready(function () {
   // create a scene
   var scene = new ScrollMagic.Scene({
       duration: 300, // the scene should last for a scroll distance of 100px
-      offset: 200 // start this scene after scrolling for 50px
+      offset: 20 // start this scene after scrolling for 50px
     })
-    .setClassToggle("#gallery", "blue")
+    .setTween(".thumbnail1", 1.5, {
+      width: "90%"
+    })
+    // .setClassToggle(".thumbnail-img", "thumbnail-img-t")
     .addIndicators()
     .addTo(controller); // assign the scene to the controller
 
-  console.log(scene.triggerElement());
 
+  // $(".thumbnail-img").animate({
+  //   width: "90%"
+  // }, 6000);
+
+
+  // console.log(scene.triggerElement());
+  console.log(scene);
 });
