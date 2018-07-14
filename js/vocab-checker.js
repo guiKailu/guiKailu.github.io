4f138d0d3050902cf97e7dbfbda5e47e8bc8d4ee
Jan She <janmarkshe@gmail.com>
2018-07-14 11:40:48 +0800

    vocab checker now returns the information I need

--- js/vocab-checker.js
+++ js/vocab-checker.js
@@ -1,4 +1,27 @@
+var readingStr = "Once upon a time, exclamatorial the children went into the woods. There they saw a witch. The witch was mean. They like to laugh and eat candy and dance all day. The end. Have a good day.";
+
+var PUNCTUATION = /,|\./g;
+
 $(document).ready(function(){
+
+  function isThisWordInThere(word, vList){
+    var regex = RegExp(word);
+    var listOfVocabulary = vList.join(' ');
+    return regex.test(listOfVocabulary) ? true : false;
+  }
+
+  function wordsThatAreNotIn(vcbListArr, rdngStr){
+    var newWords = []; //array to store all the words from the reading that aren't in the vocab list.
+    var allWords = rdngStr.replace(PUNCTUATION, ''); //removes  punctuation from the reading.
+    var allWordsArr = allWords.split(' ');
+
+    for (var i = 0; i < allWordsArr.length; i++){
+      if (!isThisWordInThere(allWordsArr[i], vcbListArr)){
+        newWords.push(allWordsArr[i]);
+      }
+    }
+    console.log(newWords);
+  }
 
   $.ajax({
     url: "data/Vocab_Up_To-EEC3_U3.csv",
@@ -8,48 +31,12 @@ $(document).ready(function(){
     dataType: "text",
     complete: function () {
         // call a function on complete
-        var vocabList = data.map(function(d){
+        var vocabListArr = data.map(function(d){
           return d[2];
         });
-        console.log(vocabList);
-
-        mammoth.extractRawText({path: "data/CC_Test_Story.docx"})
-        .then(function(result){
-        var text = result.value;
-        console.log(text);
-        var messages = result.messages;
-    })
+        // mammoth.extractRawText...
+        var newWordsArr = wordsThatAreNotIn(vocabListArr, readingStr);
+    }})
     .done();
 
-    }
-  });
 });
-
-// readFileInputEventAsArrayBuffer() is from Mammoth.js
-// function readFileInputEventAsArrayBuffer(event, callback) {
-//     var file = event.target.files[0];
-//
-//     var reader = new FileReader();
-//
-//     reader.onload = function(loadEvent) {
-//         var arrayBuffer = loadEvent.target.result;
-//         callback(arrayBuffer);
-//     };
-//
-//     reader.readAsArrayBuffer(file);
-// }
-
-// map through the  data and remove columns one and two.
-
-function readFileInputEventAsArrayBuffer(event, callback) {
-    var file = event.target.files[0];
-
-    var reader = new FileReader();
-
-    reader.onload = function(loadEvent) {
-        var arrayBuffer = loadEvent.target.result;
-        callback(arrayBuffer);
-    };
-
-    reader.readAsArrayBuffer(file);
-}
