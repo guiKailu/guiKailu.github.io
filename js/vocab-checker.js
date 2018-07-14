9256bcd19b0c4f708634f989d102d5cbdbc6ce81
Jan She <janmarkshe@gmail.com>
2018-07-14 09:30:16 +0800

    added mammoth.js and sample story

--- js/vocab-checker.js
+++ js/vocab-checker.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-console.log('hi');
+
   $.ajax({
     url: "data/Vocab_Up_To-EEC3_U3.csv",
     success: function (csvd) {
@@ -8,15 +8,48 @@ console.log('hi');
     dataType: "text",
     complete: function () {
         // call a function on complete
-        var vocabList = data.filter(function(d){
+        var vocabList = data.map(function(d){
           return d[2];
         });
         console.log(vocabList);
+
+        mammoth.extractRawText({path: "data/CC_Test_Story.docx"})
+        .then(function(result){
+        var text = result.value;
+        console.log(text);
+        var messages = result.messages;
+    })
+    .done();
+
     }
-
-
+  });
 });
 
-});
+// readFileInputEventAsArrayBuffer() is from Mammoth.js
+// function readFileInputEventAsArrayBuffer(event, callback) {
+//     var file = event.target.files[0];
+//
+//     var reader = new FileReader();
+//
+//     reader.onload = function(loadEvent) {
+//         var arrayBuffer = loadEvent.target.result;
+//         callback(arrayBuffer);
+//     };
+//
+//     reader.readAsArrayBuffer(file);
+// }
 
 // map through the  data and remove columns one and two.
+
+function readFileInputEventAsArrayBuffer(event, callback) {
+    var file = event.target.files[0];
+
+    var reader = new FileReader();
+
+    reader.onload = function(loadEvent) {
+        var arrayBuffer = loadEvent.target.result;
+        callback(arrayBuffer);
+    };
+
+    reader.readAsArrayBuffer(file);
+}
