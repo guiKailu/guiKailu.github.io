9840e26e25082784c4b2c88bfb40b2fe56ddbcc8
Jan She <janmarkshe@gmail.com>
2018-07-21 14:49:53 +0800

    made axis for fcc scatterplot challenge

--- d3-demos/js/scatterplot-graph.js
+++ d3-demos/js/scatterplot-graph.js
@@ -6,7 +6,12 @@ var margin = {
 };
 
 var width = 600 + margin.left + margin.right;
-var height = 600 + margin.top + margin.bottom;
+var height = 400 + margin.top + margin.bottom;
+
+var parseMinSec = d3.timeParse("%M:%S");
+var parseYear = d3.timeParse("%Y");
+var formatMinSec = d3.timeFormat("%M:%S");
+
 
 var g = d3.select("#chart-area")
   .append("svg")
@@ -14,3 +19,46 @@ var g = d3.select("#chart-area")
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
   .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+
+var x = d3.scaleTime()
+  .range([0, width]);
+
+var y = d3.scaleTime()
+  .range([0, height]);
+
+var xAxisGroup = g.append("g")
+  .attr("class", "x-axis")
+  .attr("id", "x-axis")
+  .attr("transform", "translate(0, " + height + ")");
+
+var yAxisGroup = g.append("g")
+  .attr("class", "y-axis")
+  .attr("id", "y-axis");
+
+d3.json("data/cyclist-data.json").then(function(data){
+
+  console.log(data);
+
+  x.domain([d3.min(data, function(d){
+    return parseYear(d.Year);
+  }), d3.max(data, function(d){
+    return parseYear(d.Year);
+  })]);
+
+  y.domain([d3.min(data, function(d){
+    return parseMinSec(d.Time);
+  }), d3.max(data, function(d){
+    return parseMinSec(d.Time);
+  })]);
+
+  var xAxisCall = d3.axisBottom(x);
+  xAxisGroup.call(xAxisCall);
+
+  var yAxisCall = d3.axisLeft(y)
+    .tickFormat(function(d){
+      return formatMinSec(d);
+    });
+
+  yAxisGroup.call(yAxisCall);
+
+});
