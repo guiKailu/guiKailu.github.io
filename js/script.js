4460ea3320d3e0b4279de0877edf8ae9eb088569
Jan She <janmarkshe@gmail.com>
2018-08-02 00:00:54 +0800

    added to portfolio: pargaraphs, button to reveal gallery, list of tools used

--- js/script.js
+++ js/script.js
@@ -1,40 +1,47 @@
 var TAGLINE_DELAY = 60;
 
+function showGallery(){
+  $("#gallery").show()
+    .animate({
+    opacity: "1"
+  }, 1000);
+}
+
 $(document).ready(function () {
 
-  // init controller
-  var controller = new ScrollMagic.Controller();
-
-  // create a scene
-  var scene1 = new ScrollMagic.Scene({
-      offset: 1
-    })
-    .setTween(".thumbnail1", {
-      width: "90%"
-    })
-    .addTo(controller);
-
-  var scene2 = new ScrollMagic.Scene({
-      offset: 2,
-    })
-    .setTween("#gallery", {
-      marginTop: "50px"
-    })
-    .addTo(controller);
-
-  var sceneTagline = new ScrollMagic.Scene({
-      duration: 88
-    })
-    .setPin("#tagline")
-    .addTo(controller);
-
-  var slowGallery = new ScrollMagic.Scene({
-      offset: 20,
-      duration: 200
-    })
-    .setPin("#gallery")
-    .addTo(controller);
-
-  // console.log(scene.triggerElement());
-  console.log(scene2);
+  // // init controller
+  // var controller = new ScrollMagic.Controller();
+  //
+  // // create a scene
+  // var scene1 = new ScrollMagic.Scene({
+  //     offset: 1
+  //   })
+  //   .setTween(".thumbnail1", {
+  //     width: "90%"
+  //   })
+  //   .addTo(controller);
+  //
+  // var scene2 = new ScrollMagic.Scene({
+  //     offset: 2,
+  //   })
+  //   .setTween("#gallery", {
+  //     marginTop: "50px"
+  //   })
+  //   .addTo(controller);
+  //
+  // var sceneTagline = new ScrollMagic.Scene({
+  //     duration: 88
+  //   })
+  //   .setPin("#tagline")
+  //   .addTo(controller);
+  //
+  // var slowGallery = new ScrollMagic.Scene({
+  //     offset: 20,
+  //     duration: 200
+  //   })
+  //   .setPin("#gallery")
+  //   .addTo(controller);
+  //
+  // // console.log(scene.triggerElement());
+  // // console.log(scene2);
 });
