d736b5e40271f194e49908384103250099f60ecc
Jan She <janmarkshe@gmail.com>
2018-08-03 10:20:18 -0700

    made version of vocab-checker that acommodates weird formmating of spreadsheet

--- js/vocab-checker.js
+++ js/vocab-checker.js
@@ -1,4 +1,4 @@
-var readingStr = 'The kids clap. Wow, you so good at soccer! You so good! Now Jordan has many friends.'.toLowerCase();
+var readingStr = 'I’m Nick. And this is my friend Civa. He is a robot. Civa goes to school with me. We play together. It is winter. It snows outside. I have a big blue jacket. The jacket is warm. Amy has a green sweater. She is not cold. Civa does not have clothing. Robots do have clothing. Grandma says, Civa! You can play in the snow. It cold. You do have a sweater. Civa says, Robots do wear sweaters! Civa plays with Amy and Nick. Nono jumps in the snow too. The snow is white. They are happy. The next day, Amy and Nick eat breakfast. Where is Civa? Civa in bed. He has the flu? Who? Civa has the flu? Civa sneezes: Achoo. ACHOOOO. ACHO01010001110111000001! Amy says, Robots do get the flu! Nick laughs. He says, Civa is a special robot. Grandma makes a red sweater for Civa. She says, Robots do wear sweaters. But Civa is a special robot. Five days later, Civa is healthy again. Grandma says, This sweater is for you. Civa laughs and says, Robots do wear sweaters. But I’m a special robot! Civa, Nick, Amy, and Nono play in the snow. Civa wears the red sweater.'.toLowerCase();
 
 var PUNCTUATION = /,|\./g;
 
@@ -25,16 +25,27 @@ $(document).ready(function(){
   }
 
   $.ajax({
-    url: "data/Vocab_EEC1_to_EEC4U3.csv",
+    url: "data/EECS_Book_1and2.csv",
     success: function (csvd) {
         data = $.csv.toArrays(csvd);
     },
     dataType: "text",
     complete: function () {
         // call a function on complete
-        var vocabListArr = data.map(function(d){
+        var vocabCellArr = data.map(function(d){
           return d[2].toLowerCase();
         });
+        var vocabListArr = [];
+        for (var i = 0; i < vocabCellArr.length; i++){
+          var tempArr = vocabCellArr[i].split(/\s|\W/);
+          for (var j = 0; j < tempArr.length; j++){
+            if (tempArr[j].length > 2 && !/\d/.test(tempArr[j]) ) {
+              vocabListArr.push(tempArr[j].replace(/\W/i, ""));
+            }
+          }
+        }
+
+        console.log(vocabListArr);
         // mammoth.extractRawText...
         var newWordsArr = wordsThatAreNotIn(vocabListArr, readingStr);
     }})
