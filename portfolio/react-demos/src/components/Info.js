fee5937414f735d368ae981372ccc955278c1faa
Jan She <janmarkshe@gmail.com>
2018-09-16 09:52:39 -0700

    imported react projects from jsfiddle

---
+++ portfolio/react-demos/src/components/Info.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Info extends Component {
+  render(){
+    return (
+        <div>
+          <h1>The Info</h1>
+          <p>React.js is awesome!</p>
+        </div>
+    );
+  }
+}
+
+export default Info;
