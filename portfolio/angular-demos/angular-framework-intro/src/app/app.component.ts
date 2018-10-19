02d49a5b5ea71cd7e66d66d6aac0206cab9e6b03
Jan She <janmarkshe@gmail.com>
2018-10-19 12:54:09 -0700

    completed more angular challenges on udemy

--- portfolio/angular-demos/angular-framework-intro/src/app/app.component.ts
+++ portfolio/angular-demos/angular-framework-intro/src/app/app.component.ts
@@ -7,8 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'Yann';
+  elements: number[] = [];
+  message = '';
+  hobbies: string[] = ['hiking', 'cooking', 'reading'];
+  hobby = '';
 
   onChangeName() {
     this.name = "Mikaela";
+  }
+
+  onAddElement(){
+    this.elements.push(this.elements.length + 1);
+  }
+
+  getColor(element: number){
+    return element  % 2 === 0 ? 'green' : 'red';
+  }
+
+  onUserWasClicked(usrName: string) {
+    alert(usrName);
+  }
+
+  onAddHobby() {
+    this.hobbies.push(this.hobby);
   }
 }
